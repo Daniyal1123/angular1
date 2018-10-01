b1df4321f77170433272e9d988ce56cb266d9838
Daniyal1123 <daniyalaslam36@gmail.com>
2018-10-01 19:00:40 +0500

    New Update with Route

--- Task2/src/app/app.module.ts
+++ Task2/src/app/app.module.ts
@@ -5,17 +5,25 @@ import { AppComponent } from './app.component';
 import { DataSetsComponent } from './data-sets/data-sets.component';
 import { NavComponent } from './nav/nav.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataSetsComponent,
     NavComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    LoginComponent,
+    SignUpComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
